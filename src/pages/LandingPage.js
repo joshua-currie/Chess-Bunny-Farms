83842f63c2ca42940c759e25b0aaf17c1ed520db
Joshua Currie <johndoe@example.com>
2023-10-26 19:59:26 -0400

    git init

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/landingpage.css';
+
+
+const LandingPage = () => {
+    return ( 
+        <div>hello</div>
+     );
+}
+ 
+export default LandingPage;
